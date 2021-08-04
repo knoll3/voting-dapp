37bb6ee5cbb37801829b86e99fd108311bcff475
Kyle Noll <noll.kyle@gmail.com>
2021-08-03 21:12:24 -0700

    Add material-ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useWeb3 } from "hooks/useWeb3";
 import { useStorageValue } from "hooks/useStorageValue";
+import { Box, Button } from "@material-ui/core";
 
 function App() {
     const web3 = useWeb3();
     const storageValue = useStorageValue(web3, 5);
+
+    const onClickButton = (event: React.MouseEvent<HTMLButtonElement>) => {
+        alert("Clicked a Material-UI button");
+    };
 
     return (
         <div className="App">
@@ -16,6 +21,15 @@ function App() {
                     Edit <code>src/App.tsx</code> and save to reload.
                 </p>
                 <div>The stored value is: {storageValue}</div>
+                <Box mt={3}>
+                    <Button
+                        onClick={onClickButton}
+                        variant="contained"
+                        color="primary"
+                    >
+                        MUI Button
+                    </Button>
+                </Box>
             </header>
         </div>
     );
