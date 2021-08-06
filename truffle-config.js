38cf806fd4aff036b271950b8ae8442b1f5698d8
Kyle Noll <noll.kyle@gmail.com>
2021-08-05 19:47:57 -0700

    Update truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -5,8 +5,13 @@ module.exports = {
     // to customize your Truffle configuration!
     contracts_build_directory: path.join(__dirname, "client/src/contracts"),
     networks: {
-        develop: {
+        development: {
+            host: "127.0.0.1",
             port: 7545,
+            network_id: "*", // Match any network id
+        },
+        develop: {
+            port: 8545,
         },
     },
 };
