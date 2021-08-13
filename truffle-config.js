bf793fe7ef4c26e481ead3e554575720490e1ac0
Kyle Noll <noll.kyle@gmail.com>
2021-08-12 22:03:34 -0700

    Deploy on ropsten

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,8 @@
 const path = require("path");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const fs = require("fs");
+
+const { projectId, mnemonic } = JSON.parse(fs.readFileSync(".secrets.json"));
 
 module.exports = {
     // See <http://truffleframework.com/docs/advanced/configuration>
@@ -12,6 +16,16 @@ module.exports = {
         },
         develop: {
             port: 8545,
+        },
+        ropsten: {
+            provider: function () {
+                return new HDWalletProvider(
+                    mnemonic,
+                    `https://ropsten.infura.io/v3/${projectId}`
+                );
+            },
+            network_id: 3,
+            gas: 4000000, //make sure this gas allocation isn't over 4M, which is the max
         },
     },
 };
