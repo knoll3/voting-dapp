cb6f471eb41d2ce6b1ea6c5c8e85d05e7fd6a22f
Kyle Noll <noll.kyle@gmail.com>
2021-08-15 10:29:43 -0700

    Simplify useWeb3 hook

--- client/src/hooks/useWeb3.ts
+++ client/src/hooks/useWeb3.ts
@@ -6,7 +6,6 @@ import Web3 from "web3";
 declare global {
     interface Window {
         ethereum: any;
-        web3: any;
     }
 }
 
@@ -18,28 +17,14 @@ export function useWeb3(): Web3 | null {
 
     useEffect(() => {
         (async () => {
-            const ethereum = window.ethereum;
-            const windowWeb3 = window.web3;
+            if (!window.ethereum) return;
 
-            if (ethereum) {
-                // Modern dapp browsers...
-                const _web3 = new Web3(ethereum);
-                try {
-                    ethereum.request({ method: "eth_requestAccounts" });
-                    setWeb3(_web3);
-                } catch (error) {
-                    console.error(error);
-                }
-            } else if (windowWeb3) {
-                // Use Mist/MetaMask's provider.
-                setWeb3(windowWeb3);
-            } else {
-                // Fallback to localhost; use dev console port by default...
-                const provider = new Web3.providers.HttpProvider(
-                    "http://127.0.0.1:7545"
-                );
-                const _web3 = new Web3(provider);
+            const _web3 = new Web3(window.ethereum);
+            try {
+                window.ethereum.request({ method: "eth_requestAccounts" });
                 setWeb3(_web3);
+            } catch (error) {
+                console.error(error);
             }
         })();
     }, []);
